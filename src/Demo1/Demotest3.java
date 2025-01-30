f55cdbdae4ea64304c6b98abbbfd4e7cdb3783fc
devjamie <dev.j.pewu@gmail.com>
2025-01-29 21:11:12 -0500

    bbb

--- src/Demo1/Demotest3.java
+++ src/Demo1/Demotest3.java
@@ -6,6 +6,20 @@ public class Demotest3 {
 	public static void main(String[] args) {
 		
 		System.out.println("Java");
+		
+
+		int a = 10;
+		int b = 20;
+		
+		int result = a+b;
+		
+		System.out.println(result);
+	}
+
+	
+	public void add (int a, int b)  {
+		System.out.println(a+b);
+	
 	}
 
 
